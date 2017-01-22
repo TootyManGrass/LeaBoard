882e95881033c1110f964d94431406c89975d5c3
Hongyu Wang <hongyu.wang@mail.utoronto.ca>
2017-01-21 19:58:59 -0500

    Files

---
+++ LeapMotionTest/src/DataGram.js
@@ -0,0 +1,9 @@
+module.exports = {
+
+	DatagramConstructor : function(id, data){
+		this.id = id;
+		this.data = data;
+	}
+
+
+}
--- LeapMotionTest/src/Server.js
+++ LeapMotionTest/src/Server.js
@@ -1,7 +1,58 @@
 var net = require('net');
 
-var server = net.createServer((socket) => {
-	
-});
 
-server.listen(5000, '127.0.0.1');
+var globalID = 0;
+var clients = [];
+var backLog = [];
+var removal = [];
+
+var Datagram = require("./DataGram.js").DatagramConstructor
+
+
+function broadCast(){
+	clients.forEach((client)=>{
+		if (!client.disconnected){
+			for (var i in backLog){
+				var data = backLog[i];
+				if (data.id != client.id)
+					client.write();
+			}
+		}
+	});
+	removal.forEach((client)=>{
+		var clientIndex = clients.indexOf(client);
+		clients.splice(clientIndex, 1);
+	})
+	removal.splice(0, removal.length);
+}
+
+setInterval(broadCast,100);
+
+//Creates a server
+net.createServer((socket)=>{
+
+	console.log("Connection from: ", socket.remotePort);
+	clients.push(socket)
+
+
+	socket.id = globalID ++;
+	socket.disconnected = false;
+
+	socket.on('data', (data) => {
+		var tmp = data.toString();
+		console.log(tmp);
+		backLog.push(new Datagram(socket.id, tmp));
+	});
+
+	socket.on('disconnect', function() {
+      	console.log('Got disconnect!');
+      	socket.disconnected = true;
+   		removal.add(socket);
+   	});
+
+
+
+}).listen(5000, '127.0.0.1');
+
+console.log("Server running port 5000\n");
+
