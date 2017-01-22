28a320c564b7070b133b6f6a135bbd3194486f4c
Shichen Lu <shichenl2@gmail.com>
2017-01-21 20:59:36 -0500

    Keyset 1 Visual Response Implementation
    -Keys light up when pressed

---
+++ VR Test/Assets/Scripts/ButtonResponse.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonResponse : MonoBehaviour {
+	public Material activeMat;
+	private Material defaultMat;
+	private MeshRenderer gameObjectRenderer;
+
+	void Start() {
+		gameObjectRenderer = GetComponent<MeshRenderer> ();
+		defaultMat = gameObjectRenderer.material;
+	}
+
+	void OnTriggerEnter (Collider other) {
+		if (other.name == "bone3" && other.transform.parent.name == "index" && other.transform.parent.transform.parent.name == "RigidRoundHand_R") {
+		Debug.Log (other.name);	
+		Debug.Log (other.transform.parent.name);
+		gameObjectRenderer.material = activeMat;
+		}
+	}
+
+	void OnTriggerExit (Collider other) {
+		if (other.name == "bone3" && other.transform.parent.name == "index" && other.transform.parent.transform.parent.name == "RigidRoundHand_R") {
+			gameObjectRenderer.material = defaultMat;
+		}
+	}
+}
