317563fa43abffc4bce4a6f62815a32ff7716be3
rayfang-ongithub <username@users.noreply.github.com>
2017-01-21 21:18:01 -0500

    facebook stuff

---
+++ facebook/FacebookThing.js
@@ -0,0 +1,33 @@
+/**
+ * Created by Ray on 2017-01-21.
+ */
+
+var readline = require('readline');
+var login = require("facebook-chat-api");
+
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("What is your email and password? \n", function(answer) {
+    var string = answer.split(' ');
+    var email = string[0];
+    var password = string[1];
+    console.log("Thank you for your valuable feedback:", answer);
+
+    login({email: email, password: password}, function callback (err, api) {
+        if(err) {
+            return console.error(err);
+        }
+        else{
+            api.getUserID("Tony Kung", function(err, data) {
+                if(err) return callback(err);
+                var threadID = data[0].userID;
+                api.sendMessage('testing1', threadID);
+            });
+        }
+    });
+    rl.close();
+    process.stdin.destroy();
+});
